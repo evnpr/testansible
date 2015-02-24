ab0e17f7f227167eccde1fd339e33f073dbd6f4d
evnpr <evanpurnama5@gmail.com>
2015-02-25 06:52:41 +0700

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,16 @@ set :deploy_to, '/root/testansible'
 set :log_level, :debug
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{tmp/sockets log config/puma}
-set :sockets_path, Pathname.new("#{fetch(:deploy_to)}/shared/tmp/sockets/")
+#set :sockets_path, Pathname.new("#{fetch(:deploy_to)}/shared/tmp/sockets/")
+set :sockets_path, Pathname.new("/var/run/")
 
 # These puma settings are only here because capistrano-puma is borked.
 # See issue #4.
 set :puma_roles, :app
-set :puma_socket, "unix://#{fetch(:sockets_path).join('puma_' + fetch(:application) + '.sock')}"
-set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl_' + fetch(:application) + '.sock')}"
+#set :puma_socket, "unix://#{fetch(:sockets_path).join('puma_' + fetch(:application) + '.sock')}"
+#set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl_' + fetch(:application) + '.sock')}"
+set :puma_socket, "unix://#{fetch(:sockets_path).join('puma.sock')}"
+set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl.sock')}"
 set :puma_state, fetch(:sockets_path).join('puma.state') 
 set :puma_log, -> { shared_path.join("log/puma-#{fetch(:stage )}.log") }
 set :puma_flags, nil 
